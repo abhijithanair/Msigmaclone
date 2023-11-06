959fe6dc0159e61d1024aa238ea923eb9a86cd67
Abhijith A Nair <abhijith.nair@experionglobal.local>
2023-11-06 21:49:02 +0530

    fix-: made changes to the user interface

--- fetch.js
+++ fetch.js
@@ -1,69 +1,47 @@
-const apiURL = "https://api.msigma.in/btech/v2/branches/";
-
-// Function to generate a random light color with half transparency
-function getRandomLightColorWithTransparency() {
-  const letters = "89ABCDEF"; // Use lighter color letters
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 8)]; // Use the first 8 letters for lighter colors
-  }
-  return color + "80"; // Add 80 (hex for 50% transparency) to the color
-}
-
-// Function to generate a random dark text color
-function getRandomDarkTextColor() {
-  const letters = "0123456789ABCDEF"; // Use all letters for darker colors
-  let color = "#";
-  for (let i = 0; i < 6; i++) {
-    color += letters[Math.floor(Math.random() * 16)];
-  }
-  return color;
-}
-
-fetch(apiURL)
-  .then((response) => {
-    if (response.ok) {
-      return response.json();
-    } else {
-      throw new Error("Failed to fetch data from the API");
-    }
-  })
+const colorarray = [
+  "#4780BB",
+  "#f6a539",
+  "#f69346",
+  "#55b8bc",
+  "#39a764",
+  "#71c2c6",
+  "#39a764",
+];
+fetch("https://api.msigma.in/btech/v2/branches/")
+  .then((response) => response.json())
   .then((data) => {
-    if (data.status === "success") {
-      const branches = data.branches;
-      branches.slice(0, 10).forEach((branch) => {
-        var container = document.querySelector(".container");
-        const branchId = branch.id;
-        const branchName = branch.name;
-        const branchShort = branch.short;
-        var divItem = document.createElement("div");
-        var bN = document.createElement("p");
-        var fees = document.createElement("p");
-        divItem.id = "div-item";
-        var p = document.createElement("a");
-        p.innerHTML = '<button id="btn">Apply now</button>';
-        bN.textContent = branchName;
+    console.log(data);
+    for (let j = 0; j < 5; j++) {
+      const mainDiv = document.createElement("div"); //Main div created
+      mainDiv.className = "section";
+      for (let i = 2 * j; i <= 2 * j + 1; i++) {
+        const newDiv = document.createElement("div"); // Create a new <div> element
+        newDiv.className = "left";
+        // Create a new element
+        let colored = Math.floor(Math.random() * colorarray.length);
+        let newHeading = document.createElement("h1");
+        newHeading.className = "Heading";
+        newHeading.textContent = data.branches[i].short;
+        newHeading.style.color = colorarray[colored];
+        let newLine = document.createElement("p");
+        newLine.textContent = data.branches[i].name;
+        let newButton = document.createElement("button");
+        newButton.textContent = "Apply Now";
+        newButton.style.color = colorarray[colored];
+        newButton.style.border = `1px solid ${colorarray[colored]}`;
+        let newPara = document.createElement("p");
+        newPara.textContent = "Fee starting at ₹799 per subject";
 
-        fees.textContent = "Fee starting at ₹799 per subject";
-        divItem.className = "item";
-        divItem.textContent = branchShort;
-        divItem.appendChild(bN);
-        divItem.appendChild(p);
-        divItem.appendChild(fees);
-        container.appendChild(divItem);
+        // Append the new <h1> element to the new <div>
+        newDiv.appendChild(newHeading);
+        newDiv.appendChild(newLine);
 
-        // Change the button color to a random light color with transparency
-        const button = divItem.querySelector("button");
-        const randomButtonColor = getRandomLightColorWithTransparency();
-        button.style.background = `linear-gradient(to bottom, ${randomButtonColor}, ${randomButtonColor})`;
+        newDiv.appendChild(newButton);
+        newDiv.appendChild(newPara);
 
-        // Change the text color for branchShort to a random dark color
-        divItem.style.color = getRandomDarkTextColor();
-      });
-    } else {
-      console.error("API returned an error status.");
+        // Append the new <div> to an existing element (e.g., body)
+        mainDiv.appendChild(newDiv);
+      }
+      document.body.appendChild(mainDiv);
     }
-  })
-  .catch((error) => {
-    console.error(error.message);
   });
